8ef7d26907f22800514e9144239e7af6637557ca
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-03-22 21:49:07 +0100

    added Character details

---
+++ src/pages/Components/CharacterDetails.svelte
@@ -0,0 +1,13 @@
+<script> 
+import  {currentCharacter} from "../../storeUser"
+</script>
+
+<div class="bg-slate-400 text-white">
+    {#if $currentCharacter !== null && $currentCharacter.name!== undefined}
+    <h2>Character's name: {$currentCharacter.name}</h2>
+    <p>Level: {$currentCharacter.level}</p>
+    <p>Race: {$currentCharacter.race}</p>
+    <p>Class: {$currentCharacter.class}</p>
+    <p>Alignment: {$currentCharacter.alignment}</p>
+    {/if}
+</div>
--- src/pages/Homepage.svelte
+++ src/pages/Homepage.svelte
@@ -1,8 +1,10 @@
 <script>
+  import CharacterDetails from "./Components/CharacterDetails.svelte";
     import Stats from "./Components/Stats.svelte";
 </script>
 
 
 <div>
+    <CharacterDetails/>
     <Stats />
 </div>
