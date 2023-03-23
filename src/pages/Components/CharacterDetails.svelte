3ec8b0b0508edc351d7249201f814440d37e96ba
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-03-23 22:55:46 +0100

    updating details

--- src/pages/Components/CharacterDetails.svelte
+++ src/pages/Components/CharacterDetails.svelte
@@ -1,29 +1,132 @@
-<script> 
+<script>
 import  {currentCharacter} from "../../storeUser"
 
+function updateDetails(e, field) {
+    e.preventDefault();
+
+          currentCharacter.update((character) => {
+      return { ...character, [field]: e.target.value };
+    });
+    
+    console.log($currentCharacter);
+}
 
 
 
-
+    
 </script>
 
-<div  class="bg-slate-400 text-white">
-    {#if $currentCharacter !== null && $currentCharacter.name!== undefined}
-    <p class="flex gap-2 group">Character's name: <input class="bg-transparent cursor-pointer max-w-max" value={$currentCharacter.name ? $currentCharacter.name : ''} type="text"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hidden group-hover:block">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-</svg>
-</p>
-    <p class="flex gap-2 group">Level: <input class="bg-transparent cursor-pointer" value={$currentCharacter.level ? $currentCharacter.level : ''} type="text"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hidden group-hover:block">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-</svg></p>
-    <p class="flex gap-2 group">Race: <input class="bg-transparent cursor-pointer" value={$currentCharacter.race ? $currentCharacter.race : ''} type="text"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hidden group-hover:block">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-</svg></p>
-    <p class="flex gap-2 group">Class: <input class="bg-transparent cursor-pointer" value={$currentCharacter.class ? $currentCharacter.class : ''} type="text"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hidden group-hover:block">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-</svg></p>
-    <p class="flex gap-2 group">Alignment: <input class="bg-transparent cursor-pointer" value={$currentCharacter.alignment ? $currentCharacter.alignment : ''} type="text"><svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="w-6 h-6 hidden group-hover:block">
-  <path stroke-linecap="round" stroke-linejoin="round" d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10" />
-</svg></p>
-    {/if}
+<div class="bg-slate-400 text-white">
+  {#if $currentCharacter !== null && $currentCharacter.name !== undefined}
+    <p class="flex gap-2 group">
+      Character's name: <input
+        on:change={(e) => updateDetails(e, 'name')}
+        style="width:{$currentCharacter.name.length}ch"
+        class="bg-transparent cursor-pointer"
+        value={$currentCharacter.name ? $currentCharacter.name : ""}
+        type="text"
+      /><svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 hidden group-hover:block"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+        />
+      </svg>
+    </p>
+    <p class="flex gap-2 group">
+      Level: <input
+      on:change={(e) => updateDetails(e, 'level')}
+        style="width:{$currentCharacter.level.length}ch"
+        class="bg-transparent cursor-pointer"
+        value={$currentCharacter.level ? $currentCharacter.level : ""}
+        type="text"
+      /><svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 hidden group-hover:block"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+        />
+      </svg>
+    </p>
+    <p class="flex gap-2 group">
+      Race: <input
+      on:change={(e) => updateDetails(e, 'race')}
+        style="width:{$currentCharacter.race.length}ch"
+        class="bg-transparent cursor-pointer"
+        value={$currentCharacter.race ? $currentCharacter.race : ""}
+        type="text"
+      /><svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 hidden group-hover:block"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+        />
+      </svg>
+    </p>
+    <p class="flex gap-2 group">
+      Class: <input
+      on:change={(e) => updateDetails(e, 'class')}
+        style="width:{$currentCharacter.class.length}ch"
+        class="bg-transparent cursor-pointer"
+        value={$currentCharacter.class ? $currentCharacter.class : ""}
+        type="text"
+      /><svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 hidden group-hover:block"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+        />
+      </svg>
+    </p>
+    <p class="flex gap-2 group">
+      Alignment: <input
+      on:change={(e) => updateDetails(e, 'alignment')}
+        style="width:{$currentCharacter.alignment.length}ch"
+        class="bg-transparent cursor-pointer"
+        value={$currentCharacter.alignment ? $currentCharacter.alignment : ""}
+        type="text"
+      /><svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-6 h-6 hidden group-hover:block"
+      >
+        <path
+          stroke-linecap="round"
+          stroke-linejoin="round"
+          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+        />
+      </svg>
+    </p>
+  {/if}
 </div>
