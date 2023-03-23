4ef400a12a5a59f502e6165330d69d0e2471e347
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-03-23 11:39:51 +0100

    stats halfway there, but has problems

--- src/pages/Components/Stats.svelte
+++ src/pages/Components/Stats.svelte
@@ -1,26 +1,28 @@
 <script>
     import  {currentCharacter} from "../../storeUser"
+    import {userData} from "../../storeUser"
 const skills = [
-  'Acrobatics (Dex)',
-  'Animals Handling (Str)',
-  'Arcana (Int)',
-  'Athletics (Str)',
-  'Deception (Cha)',
-  'History (Int)',
-  'Insight (Wis)',
-  'Intimidation (Cha)',
-  'Investigation (Int)',
-  'Medicine (Wis)',
-  'Nature (Int)',
-  'Perception (Wis)',
-  'Performance (Cha)',
-  'Persuasion (Cha)',
-  'Religion (Int)',
-  'Sleight of Hand (Dex)',
-  'Stealth (Dex)',
-  'Survival (Wis)'
+  { skill: 'Acrobatics (Dex)', value: 'acrobatics' },
+  { skill: 'Animals Handling (Str)', value: 'animals_handling' },
+  { skill: 'Arcana (Int)', value: 'arcana' },
+  { skill: 'Athletics (Str)', value: 'athletics' },
+  { skill: 'Deception (Cha)', value: 'deception' },
+  { skill: 'History (Int)', value: 'history' },
+  { skill: 'Insight (Wis)', value: 'insight' },
+  { skill: 'Intimidation (Cha)', value: 'intimidation' },
+  { skill: 'Investigation (Int)', value: 'investigation' },
+  { skill: 'Medicine (Wis)', value: 'medicine' },
+  { skill: 'Nature (Int)', value: 'nature' },
+  { skill: 'Perception (Wis)', value: 'perception' },
+  { skill: 'Performance (Cha)', value: 'performance' },
+  { skill: 'Persuasion (Cha)', value: 'persuasion' },
+  { skill: 'Religion (Int)', value: 'religion' },
+  { skill: 'Sleight of Hand (Dex)', value: 'sleight_of_hand' },
+  { skill: 'Stealth (Dex)', value: 'stealth' },
+  { skill: 'Survival (Wis)', value: 'survival' }
 ];
 
+let skillValues = [];
 
 </script>
 
@@ -30,9 +32,22 @@ const skills = [
     {/if}
     {#each skills as skill}
         <label for="" class="flex flex-row-reverse gap-2">
-            {skill}
-            <input value="" class="max-w-[3rem] bg-transparent border-b text-center" type="text">
-            <input type="checkbox">
+            {skill.skill}
+            <input value="" on:change={(e) => {
+    const inputElement = e.target;
+    const existingSkill = skillValues.find((item) => Object.keys(item)[0] === skill.value);
+
+    if (existingSkill) {
+        existingSkill[skill.value] = inputElement.value;
+    } else {
+        skillValues.push({ [skill.value]: inputElement.value });
+    }
+
+    console.log(skillValues);
+}} class="max-w-[3rem] bg-transparent border-b text-center" type="text">
+
         </label>
-    {/each}
-</div>
+        {/each}
+    </div>
+    
+    <input type="checkbox">
