c88a5019599a34e49cb52f7211a477aa39bff9c6
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-03-24 11:03:51 +0100

    so much happening right now

--- src/pages/Components/tabMenu/Actions.svelte
+++ src/pages/Components/tabMenu/Actions.svelte
@@ -7,6 +7,23 @@
   let attackPopup = false;
   let equipmentData = [];
   let selectedItemData = null;
+  let curSelectedItem = null;
+  //
+  //
+ const urls = [
+  "/api/equipment-categories/martial-melee-weapons",
+  "/api/equipment-categories/martial-ranged-weapons",
+  "/api/equipment-categories/martial-weapons",
+  "/api/equipment-categories/melee-weapons",
+  "/api/equipment-categories/ranged-weapons",
+  "/api/equipment-categories/simple-melee-weapons",
+  "/api/equipment-categories/simple-ranged-weapons",
+  "/api/equipment-categories/simple-weapons",
+  "/api/equipment-categories/staff",
+  "/api/equipment-categories/wand",
+  "/api/equipment-categories/weapon",
+  "/api/equipment-categories/shields",
+];
   //
   // Stop the popup from closing when the user clicks the children of the popup
   function handleChildClick(event) {
@@ -19,13 +36,22 @@
 
   onMount(async () => {
     try {
+    const results = await Promise.all(
+      urls.map((url) => fetch("https://www.dnd5eapi.co" + url).then((res) => res.json()))
+    );
+    equipmentData = results.flatMap((result) => result.equipment);
+    console.log(equipmentData);
+  } catch (error) {
+    console.error("Error fetching data: ", error);
+  }
+    /* try {
       const response = await fetch("https://www.dnd5eapi.co/api/equipment/");
       const data = await response.json();
       equipmentData = data.results;
     } catch (error) {
       console.error("Error fetching equipment data:", error);
     }
-    console.log(equipmentData);
+    console.log(equipmentData); */
   });
 
   async function fetchSelectedItemData(url) {
@@ -86,10 +112,10 @@
       </div>
       <div>
         <span>Search for Attacks</span>
-        {#if equipmentData.length > 0}
+        {#if equipmentData.length >= 0}
           <ul class="h-full overflow-auto pb-4">
-            {#each equipmentData as item}
-              <li on:click={() => fetchSelectedItemData(item.url)}>
+            {#each equipmentData as item, index}
+              <li class="cursor-pointer hover:bg-gray-100 p-1 rounded {curSelectedItem == index && 'bg-gray-100'}" on:click={() => {fetchSelectedItemData(item.url), curSelectedItem = index}}>
                 {item.name}
               </li>
             {/each}
@@ -100,14 +126,34 @@
       </div>
       {#if selectedItemData}
   <div class="h-full overflow-auto">
-    <h2>{selectedItemData.name}</h2>
-    <p>Description: {selectedItemData.desc ? selectedItemData.desc : 'None'}</p>
-    <p>Weapon Range: {selectedItemData.weapon_range ? selectedItemData.weapon_range : 'None'}</p>
-    <p>Range: {selectedItemData.range ? selectedItemData.range.normal + ' - ' + selectedItemData.range.long : 'None'}</p>
-    <p>Damage: {selectedItemData.damage.damage_dice ? selectedItemData.damage.damage_dice : 'None'}</p>
-    <p>Damage Type: {selectedItemData.damage.damage_type ? selectedItemData.damage.damage_type.name : 'None'}</p>
-    <pre>{JSON.stringify(selectedItemData, null, 2)}</pre>
-  </div>
+  <h2>{selectedItemData.name}</h2>
+  <p>Description: {selectedItemData.desc ? selectedItemData.desc : ''}</p>
+  <p>Weapon Range: {selectedItemData.weapon_range ?? ''}</p>
+  <p>Range: {selectedItemData.range ? selectedItemData.range.normal + ' - ' + selectedItemData.range.long : ''}</p>
+  <p>Damage: {selectedItemData.damage?.damage_dice ?? ''}</p>
+  <p>Damage Type: {selectedItemData.damage?.damage_type?.name ?? ''}</p>
+  <p>Properties:
+    {#if selectedItemData.properties?.length > 0}
+    <span>{selectedItemData.properties[0]?.name +',' ?? ''}</span>
+    <span>{selectedItemData.properties[1]?.name+',' ?? ''}</span>
+    <span>{selectedItemData.properties[2]?.name+',' ?? ''}</span>
+    <span>{selectedItemData.properties[3]?.name+',' ?? ''}</span>
+    {/if}
+</p>
+ <p>Throw Range: {selectedItemData.throw_range ? selectedItemData.throw_range.normal + ' - ' + selectedItemData.throw_range.long : ''}</p>
+  <p>Two Handed Damage: {selectedItemData.two_handed_damage?.damage_dice ?? ''}</p>
+  <p>Armor Category: {selectedItemData.armor_category ?? ''}</p>
+  <p>Armor Class: 
+    {#if selectedItemData.armor_class !== undefined}
+    <span>{selectedItemData.armor_class?.base+',' ?? ''}</span>
+    <span>dex bonus: {selectedItemData.armor_class?.dex_bonus+',' ?? ''}</span>
+    <span>max bonus: {selectedItemData.armor_class?.max_bonus+',' ?? ''}</span>
+    {/if}
+</p>
+  <p>STR Minimum: {selectedItemData.str_minimum ?? ''}</p>
+  <p>Stealth Disadvantage: {selectedItemData.stealth_disadvantage ?? ''}</p>
+  <!-- <pre>{JSON.stringify(selectedItemData, null, 2)}</pre> -->
+</div>
 {/if}
     </div>
   </div>
