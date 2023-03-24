b1b2a535a2f31071c71c88ce2ce71de70ab50c6f
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-03-24 11:19:06 +0100

    changing menu

--- src/pages/Components/TabMenu.svelte
+++ src/pages/Components/TabMenu.svelte
@@ -1,6 +1,7 @@
 <script>
   import Actions from "./tabMenu/Actions.svelte";
 
+  let menuOption = 0
 
 </script>
 
@@ -8,20 +9,25 @@
 
 <div class="bg-slate-400 text-white grid p-4 col-span-2">
     <ul class="flex gap-4">
-        <button class="p-2 border border-white hover:bg-slate-500">
+        <button on:click={()=>menuOption = 0} class="p-2 border border-white hover:bg-slate-500">
             ACTIONS
         </button>
-        <button class="p-2 border border-white hover:bg-slate-500">
+        <button on:click={()=>menuOption = 1} class="p-2 border border-white hover:bg-slate-500">
             SPELLS
         </button>
-        <button class="p-2 border border-white hover:bg-slate-500">
+        <button on:click={()=>menuOption = 2} class="p-2 border border-white hover:bg-slate-500">
             FEATURES & TRAITS
         </button>
-        <button class="p-2 border border-white hover:bg-slate-500">
+        <button on:click={()=>menuOption = 3} class="p-2 border border-white hover:bg-slate-500">
             LANGUAGES & EQUIPMENT
         </button>
     </ul>
     <div class="min-h-[250px]">
+        {#if menuOption == 0}
         <Actions/>
+        {:else if menuOption == 1}
+        {:else if menuOption == 2}
+        {:else if menuOption == 3}
+        {/if}
     </div>
 </div>
