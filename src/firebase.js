5a842c46896779b876b12baf976617273294f15c
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-03-26 00:47:54 +0100

    Upload profile image

--- src/firebase.js
+++ src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 // TODO: Replace the following with your app's Firebase project configuration
 const firebaseConfig = {
@@ -16,3 +17,4 @@ export const app = initializeApp(firebaseConfig);
 // Initialize Firebase Authentication and get a reference to the service
 export const auth = getAuth(app);
 export const db = getFirestore(app);
+export const storage = getStorage(app);
--- src/pages/Components/CharacterDetails.svelte
+++ src/pages/Components/CharacterDetails.svelte
@@ -1,129 +1,200 @@
 <script>
-import  {currentCharacter} from "../../storeUser"
+  import { currentCharacter } from "../../storeUser";
+  import { storage } from "../../firebase";
+  import { fly,fade } from 'svelte/transition';
+  import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+  import { userData, googleUser } from "../../storeUser";
 
-// updates name class race level and alignment
-function updateDetails(e, field) {
+  let imageUrl = "";
+  let tooLarge = false;
+  let stockPortrait = "../../../public/stockPortrait.svg";
+
+  // updates name class race level and alignment
+  function updateDetails(e, field) {
     e.preventDefault();
 
-          currentCharacter.update((character) => {
+    currentCharacter.update((character) => {
       return { ...character, [field]: e.target.value };
     });
+    console.log($googleUser);
+    console.log($userData);
+    console.log($currentCharacter);
+  }
 
-}
-    
+  async function uploadImage(event) {
+    if (event.target.files[0].size >= 5000000) {
+      console.log("file too large");
+      iamgeToLarge()
+    } else {
+      const file = event.target.files[0];
+      const fileName = `${Date.now()}-${file.name}`; // To generate a unique file name
+      const storageRef = ref(
+        storage,
+        `images/${$googleUser.uid}/${$currentCharacter.id}/${fileName}`
+      );
+
+      try {
+        const uploadTask = uploadBytesResumable(storageRef, file);
+        await new Promise((resolve, reject) => {
+          uploadTask.on(
+            "state_changed",
+            null,
+            (error) => {
+              reject(error);
+            },
+            () => {
+              resolve(uploadTask.snapshot);
+            }
+          );
+        });
+
+        const downloadURL = await getDownloadURL(storageRef);
+        imageUrl = downloadURL;
+      } catch (error) {
+        console.error("Error uploading image:", error);
+      }
+      currentCharacter.update((character) => {
+        return { ...character, image: imageUrl };
+      });
+    }
+  }
+
+  function iamgeToLarge() {
+    tooLarge = true;
+    setTimeout(() => {
+      tooLarge = false;
+    },2000)
+  }
 </script>
 
 <div class="bg-slate-400 text-white">
   {#if $currentCharacter !== null && $currentCharacter.name !== undefined}
-    <p class="flex gap-2 group">
-      Character's name: <input
-        on:change={(e) => updateDetails(e, 'name')}
-        style="width:{$currentCharacter.name.length}ch"
-        class="bg-transparent cursor-pointer"
-        value={$currentCharacter.name ? $currentCharacter.name : ""}
-        type="text"
-      /><svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6 hidden group-hover:block"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+    <!-- Character Portrait -->
+    <div class="relative group">
+      <img
+        class="w-32 h-32 border border-black rounded bg-white object-cover"
+        src={$currentCharacter.image ? $currentCharacter.image : stockPortrait}
+        alt=""
         />
-      </svg>
-    </p>
-    <p class="flex gap-2 group">
-      Level: <input
-      on:change={(e) => updateDetails(e, 'level')}
-        style="width:{$currentCharacter.level.length}ch"
-        class="bg-transparent cursor-pointer"
-        value={$currentCharacter.level ? $currentCharacter.level : ""}
-        type="text"
-      /><svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6 hidden group-hover:block"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-        />
-      </svg>
-    </p>
-    <p class="flex gap-2 group">
-      Race: <input
-      on:change={(e) => updateDetails(e, 'race')}
-        style="width:{$currentCharacter.race.length}ch"
-        class="bg-transparent cursor-pointer"
-        value={$currentCharacter.race ? $currentCharacter.race : ""}
-        type="text"
-      /><svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6 hidden group-hover:block"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-        />
-      </svg>
-    </p>
-    <p class="flex gap-2 group">
-      Class: <input
-      on:change={(e) => updateDetails(e, 'class')}
-        style="width:{$currentCharacter.class.length}ch"
-        class="bg-transparent cursor-pointer"
-        value={$currentCharacter.class ? $currentCharacter.class : ""}
-        type="text"
-      /><svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6 hidden group-hover:block"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-        />
-      </svg>
-    </p>
-    <p class="flex gap-2 group">
-      Alignment: <input
-      on:change={(e) => updateDetails(e, 'alignment')}
-        style="width:{$currentCharacter.alignment.length}ch"
-        class="bg-transparent cursor-pointer"
-        value={$currentCharacter.alignment ? $currentCharacter.alignment : ""}
-        type="text"
-      /><svg
-        xmlns="http://www.w3.org/2000/svg"
-        fill="none"
-        viewBox="0 0 24 24"
-        stroke-width="1.5"
-        stroke="currentColor"
-        class="w-6 h-6 hidden group-hover:block"
-      >
-        <path
-          stroke-linecap="round"
-          stroke-linejoin="round"
-          d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
-        />
-      </svg>
-    </p>
+        <input class="hidden group-hover:block" type="file" accept="image/*" on:change={uploadImage} />
+      {#if tooLarge}
+      <div in:fly="{{ y: -50, duration: 300 }}" out:fade class="absolute w-32 h-10 bg-white shadow-md top-1 left-0 rounded flex items-center justify-center">
+        <span class="text-black my-auto">File too large.</span>
+      </div>
+      {/if}
+    </div>
+    <!-- Character Details -->
+    <div>
+      <p class="flex gap-2 group">
+        Character's name: <input
+          on:change={(e) => updateDetails(e, "name")}
+          style="width:{$currentCharacter.name.length}ch"
+          class="bg-transparent cursor-pointer"
+          value={$currentCharacter.name ? $currentCharacter.name : ""}
+          type="text"
+        /><svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 hidden group-hover:block"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+      </p>
+      <p class="flex gap-2 group">
+        Level: <input
+          on:change={(e) => updateDetails(e, "level")}
+          style="width:{$currentCharacter.level.length}ch"
+          class="bg-transparent cursor-pointer"
+          value={$currentCharacter.level ? $currentCharacter.level : ""}
+          type="text"
+        /><svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 hidden group-hover:block"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+      </p>
+      <p class="flex gap-2 group">
+        Race: <input
+          on:change={(e) => updateDetails(e, "race")}
+          style="width:{$currentCharacter.race.length}ch"
+          class="bg-transparent cursor-pointer"
+          value={$currentCharacter.race ? $currentCharacter.race : ""}
+          type="text"
+        /><svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 hidden group-hover:block"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+      </p>
+      <p class="flex gap-2 group">
+        Class: <input
+          on:change={(e) => updateDetails(e, "class")}
+          style="width:{$currentCharacter.class.length}ch"
+          class="bg-transparent cursor-pointer"
+          value={$currentCharacter.class ? $currentCharacter.class : ""}
+          type="text"
+        /><svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 hidden group-hover:block"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+      </p>
+      <p class="flex gap-2 group">
+        Alignment: <input
+          on:change={(e) => updateDetails(e, "alignment")}
+          style="width:{$currentCharacter.alignment.length}ch"
+          class="bg-transparent cursor-pointer"
+          value={$currentCharacter.alignment ? $currentCharacter.alignment : ""}
+          type="text"
+        /><svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke-width="1.5"
+          stroke="currentColor"
+          class="w-6 h-6 hidden group-hover:block"
+        >
+          <path
+            stroke-linecap="round"
+            stroke-linejoin="round"
+            d="M16.862 4.487l1.687-1.688a1.875 1.875 0 112.652 2.652L10.582 16.07a4.5 4.5 0 01-1.897 1.13L6 18l.8-2.685a4.5 4.5 0 011.13-1.897l8.932-8.931zm0 0L19.5 7.125M18 14v4.75A2.25 2.25 0 0115.75 21H5.25A2.25 2.25 0 013 18.75V8.25A2.25 2.25 0 015.25 6H10"
+          />
+        </svg>
+      </p>
+    </div>
   {/if}
 </div>
