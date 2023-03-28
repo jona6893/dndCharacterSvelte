1f1e231974ecc8018b92aa435d49fbaebf4b4cbb
”Jonathan <jona6893@stud.kea.dkgit config --global user.name Jonathan Weldongit config --global user.email jona6893@stud.kea.dk>
2023-03-28 22:24:58 +0200

    changed skills look

--- src/pages/Components/Skills.svelte
+++ src/pages/Components/Skills.svelte
@@ -44,18 +44,15 @@ function updateSkillValues(e, skillKey, isCheckbox = false) {
 
 </script>
 
-<div class="bg-slate-400 min-w-min w-72 grid gap-2 justify-items-start p-4 rounded text-white col-start-1">
-    {#if $currentCharacter !== null && $currentCharacter.name !== undefined}
-    <h2>Skills</h2>
-    {/if}
+<div class="bg-gray-100 min-w-min w-72 grid gap-2 justify-items-start p-4 rounded col-start-1">
     {#each skills as skill}
 
-        <label for="" class="flex flex-row-reverse gap-2">
+        <label for="" class="flex flex-row-reverse text-gray-600 gap-2">
             {skill.skill}
 <input
       value={$currentCharacter.skills && $currentCharacter.skills[skill.value] ? $currentCharacter.skills[skill.value] : ''}
       on:change={(e) => updateSkillValues(e, skill.value)}
-      class="max-w-[3rem] bg-transparent border-b text-center cursor-pointer"
+      class="max-w-[3rem] bg-transparent border-b border-gray-400 text-center cursor-pointer text-black"
       type="text"
       >
       <input type="checkbox" class="checkbox-round cursor-pointer" checked="{($currentCharacter.skills && $currentCharacter.skills[skill.checkValue]) || false}" on:change={(e) => updateSkillValues(e, skill.checkValue, true)} />
